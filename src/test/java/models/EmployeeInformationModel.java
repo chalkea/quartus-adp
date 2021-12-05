6bab86aca37f7226b4fccc249f03a17afa470c63
Allen Toole <allen.toole@adp.com>
2021-12-04 22:39:47 -0500

    add override for to toString method for printing the employee information object

--- src/test/java/models/EmployeeInformationModel.java
+++ src/test/java/models/EmployeeInformationModel.java
@@ -4,26 +4,24 @@ import com.github.javafaker.Faker;
 import utils.DateUtilities;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class EmployeeInformationModel {
-    private  String socialSecurity;
-    private  String firstName;
-    private  String middleName;
-    private  String lastName;
-    private  String namePrefix;
-    private  boolean appliedForSsn, ssnIsFederalEin, localNational;
-    private  String nameSuffix;
-    private  boolean alterDerivedName;
-    private  String address1, address2, address3, city;
-    private  String stProv, postalZip, country, dateOfResidence;
-    private  String county, phoneType, phone, emailBusiness, emailPersonal;
+    private String socialSecurity;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String namePrefix;
+    private boolean appliedForSsn, ssnIsFederalEin, localNational;
+    private String nameSuffix;
+    private boolean alterDerivedName;
+    private String address1, address2, address3, city;
+    private String stProv, postalZip, country, dateOfResidence;
+    private String county, phoneType, phone, emailBusiness, emailPersonal;
 
     public EmployeeInformationModel() {
         this.socialSecurity = Faker.instance().idNumber().ssnValid();
         this.firstName = Faker.instance().name().firstName();
-        this.middleName = Faker.instance().name().firstName();
+        this.middleName = Faker.instance().name().firstName().substring(1, 2);
         this.lastName = Faker.instance().name().lastName();
         this.namePrefix = Faker.instance().name().prefix();
         this.appliedForSsn = true;
@@ -36,45 +34,44 @@ public class EmployeeInformationModel {
         this.address3 = Faker.instance().address().buildingNumber() + " Secondary";
         this.city = Faker.instance().address().city();
         this.stProv = Faker.instance().address().stateAbbr();
-        this.postalZip = Faker.instance().address().zipCodeByState(this.stProv);
+        this.postalZip = Faker.instance().address().zipCodeByState(this.stProv).replaceAll("#", Faker.instance().number().digits(1));
         this.country = Faker.instance().address().country();
-        this.dateOfResidence = DateUtilities.adjustDate("mm/dd/yyy", -180);
-        this.county = Faker.instance().address().countyByZipCode(this.postalZip);
+        this.dateOfResidence = DateUtilities.adjustDate("dd/mm/yyy", -180);
+        this.county = "unknown";
         this.phoneType = "CEL1 - Cellular Phone 1";
         this.phone = Faker.instance().phoneNumber().cellPhone();
-        this.emailBusiness = System.getenv("user.name") + ".testBusiness@adp.com";
-        this.emailPersonal = System.getenv("user.name") + ".testPersonal@adp.com";
+        this.emailBusiness = System.getProperty("user.name") + ".testBusiness@adp.com";
+        this.emailPersonal = System.getProperty("user.name") + ".testPersonal@adp.com";
     }
 
-    public List<Map<String, String>> getEmployeeInfoDataMap() {
-        Map<String, String> employeeInformation = new HashMap<>();
+    public HashMap<String, String> getEmployeeInfoDataMap() {
+        HashMap<String, String> employeeInformation = new HashMap<>();
 
-        employeeInformation.put(this.socialSecurity, "Social Security");
-        employeeInformation.put(this.firstName, "First Name");
-        employeeInformation.put(this.middleName, "Middle Name");
-        employeeInformation.put(this.lastName, "Last Name");
-        employeeInformation.put(this.namePrefix, "Name Prefix");
-        employeeInformation.put((this.appliedForSsn ? "Yes" : "no"), "Applied For Ssn");
-        employeeInformation.put((this.ssnIsFederalEin ? "yes" : "no"), "Ssn Is Federal Ein");
-        employeeInformation.put(this.localNational ? "yes" : "no", "Local National");
-        employeeInformation.put(this.nameSuffix, "Name Suffix");
-        employeeInformation.put(this.alterDerivedName ? "yes" : "no", "Alter Derived Name");
-        employeeInformation.put(this.address1, "Address1");
-        employeeInformation.put(this.address2, "Address2");
-        employeeInformation.put(this.address3, "Address3");
-        employeeInformation.put(this.city, "City");
-        employeeInformation.put(this.stProv, "St Prov");
-        employeeInformation.put(this.postalZip, "Postal Zip");
-        employeeInformation.put(this.country, "Country");
-        employeeInformation.put(this.dateOfResidence, "Date Of Residence");
-        employeeInformation.put(this.county, "County");
-        employeeInformation.put(this.phoneType, "Phone Type");
-        employeeInformation.put(this.phone, "Phone");
-        employeeInformation.put(this.emailBusiness, "Email Business");
-        employeeInformation.put(this.emailPersonal, "Email Personal");
+        employeeInformation.put("Social Security", this.socialSecurity);
+        employeeInformation.put("First Name", this.firstName);
+        employeeInformation.put("Middle Name", this.middleName);
+        employeeInformation.put("Last Name", this.lastName);
+        employeeInformation.put("Name Prefix", this.namePrefix);
+        employeeInformation.put("Applied For Ssn", (this.appliedForSsn ? "Yes" : "no"));
+        employeeInformation.put("Ssn Is Federal Ein", (this.ssnIsFederalEin ? "yes" : "no"));
+        employeeInformation.put("Local National", this.localNational ? "yes" : "no");
+        employeeInformation.put("Name Suffix", this.nameSuffix);
+        employeeInformation.put("Alter Derived Name", this.alterDerivedName ? "yes" : "no");
+        employeeInformation.put("Address1", this.address1);
+        employeeInformation.put("Address2", this.address2);
+        employeeInformation.put("Address3", this.address3);
+        employeeInformation.put("City", this.city);
+        employeeInformation.put("St Prov", this.stProv);
+        employeeInformation.put("Postal Zip", this.postalZip);
+        employeeInformation.put("Country", this.country);
+        employeeInformation.put("Date Of Residence", this.dateOfResidence);
+        employeeInformation.put("County", this.county);
+        employeeInformation.put("Phone Type", this.phoneType);
+        employeeInformation.put("Phone", this.phone);
+        employeeInformation.put("Email Business", this.emailBusiness);
+        employeeInformation.put("Email Personal", this.emailPersonal);
 
-        return (List<Map<String, String>>) employeeInformation;
-
+        return employeeInformation;
     }
 
     public String getSocialSecurity() {
@@ -267,5 +264,35 @@ public class EmployeeInformationModel {
 
     public void setEmailPersonal(String emailPersonal) {
         this.emailPersonal = emailPersonal;
+    }
+
+    @Override
+    public String toString() {
+        String employeeInfo = (
+                "Social Security: " + this.socialSecurity
+                        + "\nFirst Name: " + this.firstName
+                        + "\nMiddle Name: " + this.middleName
+                        + "\nLast Name: " + this.lastName
+                        + "\nName Prefix: " + this.namePrefix
+                        + "\nApplied For Ssn: " + (this.appliedForSsn)
+                        + "\nSsn Is Federal Ein: " + (this.ssnIsFederalEin)
+                        + "\nLocal National: " + this.localNational
+                        + "\nName Suffix: " + this.nameSuffix
+                        + "\nAlter Derived Name: " + this.alterDerivedName
+                        + "\nAddress1: " + this.address1
+                        + "\nAddress2: " + this.address2
+                        + "\nAddress3: " + this.address3
+                        + "\nCity: " + this.city
+                        + "\nSt Prov: " + this.stProv
+                        + "\nPostal Zip: " + this.postalZip
+                        + "\nCountry: " + this.country
+                        + "\nDate Of Residence: " + this.dateOfResidence
+                        + "\nCounty: " + this.county
+                        + "\nPhone Type: " + this.phoneType
+                        + "\nPhone: " + this.phone
+                        + "\nEmail Business: " + this.emailBusiness
+                        + "\nEmail Personal: " + this.emailPersonal
+        );
+        return employeeInfo;
     }
 }
